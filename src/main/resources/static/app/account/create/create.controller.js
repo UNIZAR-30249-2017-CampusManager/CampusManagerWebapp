74d16b0846d2bea342459460f4ed77c3bcdffdf4
Fran Menendez Moya <menendezmoya94@gmail.com>
2017-03-25 11:51:56 +0100

    Funcionalidad crear usuario

--- src/main/resources/static/app/account/create/create.controller.js
+++ src/main/resources/static/app/account/create/create.controller.js
@@ -5,29 +5,56 @@
         .module('app.account.create')
         .controller('CreateController', CreateController);
 
-    //CreateController.inject = ['dependency1'];
+    CreateController.inject = ['$scope','$http','AlertService'];
 
-    function CreateController($scope) {
+    function CreateController($scope,$http,AlertService) {
         var vm = this;
 
-        //TODO: Obtener Roles de la API
         var availableOptions = [
-            {value: 'ROL_1', name: 'ROL_1_NAME'},
-            {value: 'ROL_2', name: 'ROL_2_NAME'},
-            {value: 'ROL_3', name: 'ROL_3_NAME'}
+            {value: 'ADMIN', name: 'Administrador'},
+            {value: 'WORKER', name: 'Trabajador'},
+            {value: 'PROFFESOR', name: 'Profesor'}
         ];
 
-        vm.create = create;
+        vm.crearError = false;
         vm.availableOptions = availableOptions;
+        vm.create = create;
+        vm.toggleCrear = toggleCrear;
 
         $scope.selected = {};
+
+        function toggleCrear(){
+            vm.crearError = false;
+        }
 
         function create(event) {
             event.preventDefault();
 
             if($scope.selected.value == undefined){
-                console.log("Selecciona ROL loco");
-            } else console.log("Email: " + vm.email + " , Password: " + vm.password + " , rol: " + $scope.selected.value.value);
+                //console.log("Selecciona ROL loco");
+            } else{
+                //console.log("Email: " + vm.email + " , Password: " + vm.password + " , rol: " + $scope.selected.value.value);
+
+                var data = {
+                    name: vm.name,
+                    surname: vm.surname,
+                    email: vm.email,
+                    password: vm.password,
+                    role: $scope.selected.value.value
+                };
+
+                $http.post("/api/user", data).then(
+                    function (response) { //success
+                        //console.log("Respuesta: " + response);
+                        vm.crearError = false;
+
+                        AlertService.addAlert('success','¡El usuario ' + data.email + ' ha sido registrado con éxito!')
+                    },
+                    function (response) { //error
+                        vm.crearError = true;
+                    }
+                );
+            }
         }
     }
 })();
